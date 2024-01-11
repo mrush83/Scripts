7002188442fc94f277fd95fc02e488840bca8b69
Gremlenz <53276113+mrush83@users.noreply.github.com>
2024-01-11 00:50:50 -0500

    Delete PowerShell/Workspace One directory

--- PowerShell/Workspace One/Sensors/Get_Secure_Boot_Status.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Set the execution policy for the current process to Unrestricted, allowing the execution of scripts without any restrictions.
-# This change applies only to the current script or session.
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
-
-# Check the current status of Secure Boot
-$secureBootStatus = Confirm-SecureBootUEFI
-$secureBootStatus
