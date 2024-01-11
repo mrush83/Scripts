14440dd1f5881be1936b5945f84262d06888237b
Gremlenz <53276113+mrush83@users.noreply.github.com>
2024-01-11 00:09:40 -0500

    Add files via upload

---
+++ PowerShell/Workspace One/Sensors/Get_Secure_Boot_Status.ps1
@@ -0,0 +1,7 @@
+﻿# Set the execution policy for the current process to Unrestricted, allowing the execution of scripts without any restrictions.
+# This change applies only to the current script or session.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+# Check the current status of Secure Boot
+$secureBootStatus = Confirm-SecureBootUEFI
+$secureBootStatus
