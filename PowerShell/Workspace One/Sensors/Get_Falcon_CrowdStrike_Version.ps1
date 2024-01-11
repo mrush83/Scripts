2e287a822c32742cfc817bb7f82446a175c057b4
Gremlenz <53276113+mrush83@users.noreply.github.com>
2024-01-11 00:08:42 -0500

    Add files via upload

---
+++ PowerShell/Workspace One/Sensors/Get_Falcon_CrowdStrike_Version.ps1
@@ -0,0 +1,16 @@
+﻿# Set the execution policy for the current process to Unrestricted, allowing the execution of scripts without any restrictions.
+# This change applies only to the current script or session.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+# Attempt to get the version of CrowdStrike Windows Sensor
+$package = Get-Package "CrowdStrike Windows Sensor*" -ErrorAction SilentlyContinue
+
+# Check if the package was found
+if ($package) {
+    # If found, display the version
+    $version = $package.version
+    Write-Output "Version is: $version"
+} else {
+    # If not found, display an error message
+    Write-Output "Version is: Null, Software not found"
+}
