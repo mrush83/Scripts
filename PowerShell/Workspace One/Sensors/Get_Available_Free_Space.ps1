b41ee5f0d1c8f77b20f5c89a45b1cf35fd7dd465
Gremlenz <53276113+mrush83@users.noreply.github.com>
2024-01-11 00:04:49 -0500

    Add files via upload

---
+++ PowerShell/Workspace One/Sensors/Get_Available_Free_Space.ps1
@@ -0,0 +1,7 @@
+﻿# Set the execution policy for the current process to Unrestricted, allowing the execution of scripts without any restrictions.
+# This change applies only to the current script or session.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+$driveInfo = Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk WHERE DriveType = 3 AND DeviceID = 'C:'"
+$freeSpaceGB = [math]::Round($driveInfo.FreeSpace / 1GB, 2)
+Write-Output ($freeSpaceGB.ToString("F2") + " GB") 
